e7e5e8f6070e95c626ede86c3ffbf2189a110f4b
nigelgks <nigelgan@yahoo.com>
2024-03-14 12:25:14 +0800

    increase header paddingTop

--- src/screens/Home/Profile.js
+++ src/screens/Home/Profile.js
@@ -381,7 +381,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-between',
         paddingBottom: 35,
-        paddingTop: 30
+        paddingTop: 50
     },
     headerTitle: {
         fontSize: 30,
